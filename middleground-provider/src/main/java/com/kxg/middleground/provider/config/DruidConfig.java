aa120c46cdc06ae82f97f3d22b5ea7e711570ab1
mac <806871609@qq.com>
2020-04-22 18:29:53 +0800

    open druid

--- middleground-provider/src/main/java/com/kxg/middleground/provider/config/DruidConfig.java
+++ middleground-provider/src/main/java/com/kxg/middleground/provider/config/DruidConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.support.http.StatViewServlet;
+import java.util.*;
+import java.util.HashMap;
+
 /**
  * druid的视图配置
  * http://localhost/druid/index.html
@@ -39,6 +42,17 @@ public class DruidConfig {
     //这是配置druid的监控
     @Bean
     public ServletRegistrationBean servletRegistrationBean(){
-        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("resetEnable", "false"); //禁用HTML页面上的“Rest All”功能
+      //  initParameters.put("allow", "10.8.9.115");  //ip白名单（没有配置或者为空，则允许所有访问）
+        initParameters.put("loginUsername", "admin");  //++监控页面登录用户名
+        initParameters.put("loginPassword", "admin");  //++监控页面登录用户密码
+     //   initParameters.put("deny", ""); //ip黑名单
+        //如果某个ip同时存在，deny优先于allow
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
     }
 }
