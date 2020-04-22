6c99ea8c53630ea8e72fe9b4062d0f2a400cb455
mac <806871609@qq.com>
2020-04-22 18:52:39 +0800

    短信

--- middleground-provider/src/main/java/com/kxg/middleground/provider/controller/SmsController.java
+++ middleground-provider/src/main/java/com/kxg/middleground/provider/controller/SmsController.java
@@ -2,7 +2,12 @@ package com.kxg.middleground.provider.controller;
 
 import com.kxg.middleground.provider.common.SzpJsonResult;
 import com.kxg.middleground.provider.service.SmsService;
+import com.kxg.middleground.request.SentSmsCodeRequest;
+import com.kxg.middleground.request.VerificationSmsCodeRequest;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +24,18 @@ public class SmsController {
     /**
      * 验证短信
      */
-    public SzpJsonResult<Boolean> verificationSms(){
-        return null;
+    @ApiOperation("验证短信,1成功，0失败，zues的appkey-4CE8EE38DCED46D2")
+    @PostMapping("sent")
+    public SzpJsonResult<Boolean> verificationSms(@RequestBody SentSmsCodeRequest request){
+        return SzpJsonResult.ok(smsService.sentCode(request));
     }
     /**
      * 发送短信
      */
-    public SzpJsonResult<Boolean> sendSmsCode(){
-        return null;
+    @ApiOperation("发送短信")
+    @PostMapping("ver,1成功，0失败")
+    public SzpJsonResult<Boolean> sendSmsCode(@RequestBody VerificationSmsCodeRequest request){
+        return SzpJsonResult.ok(smsService.verification(request));
     }
 
 
